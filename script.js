4e483048cc5b0de9cc7a488b47879aeb282f47b6
Suril Pokharel <surilpokharel3@gmail.com>
2024-12-17 10:17:10 +0545

    Added functionality for changing word

---
+++ script.js
@@ -0,0 +1,5 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    var typed = new typed('#element', {
+      strings: ['Frontend Developer', 'UI/UX Designer','Content Writer'],
+      typeSpeed: 50,
+    });
