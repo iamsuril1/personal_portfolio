54d46f26b6d2dce68b15b206f4fe23300b54452e
Suril Pokharel <surilpokharel3@gmail.com>
2024-12-21 10:38:54 +0545

    added feature

--- script.js
+++
@@ -1,5 +0,0 @@
-<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
-    var typed = new typed('#element', {
-      strings: ['Frontend Developer', 'UI/UX Designer','Content Writer'],
-      typeSpeed: 50,
-    });
